cf6b4a4d589a1aa98b9f6a5f1041d23ddff5e8d3
mvaldes <mvaldes.revilla@gmail.com>
2022-10-03 13:46:21 -0500

    it works now

---
+++ README.md
@@ -0,0 +1,9 @@
+# Runs terraform commands on manifests
+
+- Useful to quickly show you the results of the plan in a separate vertical window.
+
+# Usage
+- In a terraform file, call function :TerraPlan or :TerraApply
+
+# TODO
+- Prompt for potential undefined variables
--- lua/terraform/init.lua
+++ lua/terraform/init.lua
@@ -3,63 +3,50 @@ local M = {}
 
 -- Finds if the terraform binary is installed in your system
 local function terraform_binary()
-  -- System call to check if binary is installed and in PATH
-  local checker = vim.fn.systemlist('which terraform')
-  for _, v in pairs(checker) do
-    if not string.match(v, "found") then
-      print("Terraform is installed")
+  local check = vim.fn.systemlist("command -v terraform")
+  for _, v in ipairs(check) do
+    if string.match(v, '/usr/bin/terraform') then
+      return true
     end
   end
 end
 
 -- Gets current file extension
 local function get_file_extension()
-  local extension = vim.fn.expand("%:e")
-  if extension == "tf" then
+  if vim.fn.expand("%:e") == "tf" then
     return true
+  else
+    print("Not a terraform file")
   end
 end
 
--- Creates a buffer to show results of command
-local function buffer_handler(action)
-  vim.api.nvim_command("vnew")
-  vim.api.nvim_create_buf({}, {})
-  local split_buf = vim.api.nvim_get_current_buf()
-  vim.api.nvim_buf_set_name(split_buf, action)
-  vim.api.nvim_buf_set_lines(split_buf, 0, 0, false, { "Running terraform " .. action })
-  vim.cmd("cd " .. vim.fn.expand("%:h"))
-  print(vim.cmd("!pwd"))
-  vim.fn.jobstart({"ls", "-lrt"}, {
-    stdout_buffered = true,
-    stderr_buffered = true,
-    on_stdout = function(_, data)
-      if data then
-        vim.api.nvim_buf_set_lines(split_buf, -1, -1, false, data)
-      end
-    end,
-    on_stderr = function(_, data)
-      if data then
-        vim.api.nvim_buf_set_lines(split_buf, -1, -1, false, data)
-      end
-    end
-  })
-end
 
 -- Executes terraform plan
-local function terraform_action(action)
-  terraform_binary()
-  if get_file_extension() then
-    buffer_handler(action)
+local function terraform_plan()
+  if terraform_binary() and get_file_extension() then
+      vim.cmd("lcd" .. vim.fn.expand("%:p:h"))
+      -- Open terminal and send command
+      vim.api.nvim_command("ToggleTerm size=80 direction=vertical")
+      vim.api.nvim_command("TermExec cmd='terraform plan'")
+  end
+end
+
+-- Executes terraform apply
+local function terraform_apply()
+  if terraform_binary() and get_file_extension() then
+    vim.cmd("lcd" .. vim.fn.expand("%:p:h"))
+    -- Open terminal and send command
+    vim.api.nvim_command("ToggleTerm size=80 direction=vertical")
+    vim.api.nvim_command("TermExec cmd='terraform apply'")
   end
 end
 
 M.plan = function()
-  terraform_action("plan")
+  terraform_plan()
 end
 
 M.apply = function()
-  terraform_action("apply")
+  terraform_apply()
 end
-
 
 return M
