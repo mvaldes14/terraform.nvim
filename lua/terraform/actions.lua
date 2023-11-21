c60a455e8bb9f737b03e464cd4281fce740a1009
mvaldes <mvaldes.revilla@gmail.com>
2023-11-20 21:45:28 -0600

    feat: adding terraform validate

--- lua/terraform/actions.lua
+++ lua/terraform/actions.lua
@@ -5,68 +5,96 @@ local file_extension = require("terraform.helper").get_file_extension
 
 -- Global Scope so it can be reached
 local popup = Popup({
-	enter = true,
-	focusable = true,
-	border = {
-		style = "rounded",
-		text = {
-			top = "Terraform Plan",
-			top_align = "center",
-		},
-	},
-	position = "50%",
-	size = {
-		width = "80%",
-		height = "80%",
-	},
+    enter = true,
+    focusable = true,
+    border = {
+        style = "rounded",
+        text = {
+            top = "Terraform Plan",
+            top_align = "center",
+        },
+    },
+    position = "50%",
+    size = {
+        width = "80%",
+        height = "80%",
+    },
 })
 
 -- Runs terraform plan and places output in popup
 local function terraform_plan()
-	local results = {}
-	Job:new({
-		command = "terraform",
-		args = { "plan", "-json" },
-		on_exit = function(j, _)
-			for _, line in ipairs(j:result()) do
-				table.insert(results, line)
-			end
-		end,
-	}):sync(99999) -- Needs a long time if the directory has tons of files
+    local results = {}
+    Job:new({
+        command = "terraform",
+        args = { "plan", "-json" },
+        on_exit = function(j, _)
+            for _, line in ipairs(j:result()) do
+                table.insert(results, line)
+            end
+        end,
+    }):sync(99999) -- Needs a long time if the directory has tons of files
 
-	for _, v in ipairs(results) do
-		local parsed_msg = vim.json.decode(v)
-		vim.api.nvim_buf_set_lines(popup.bufnr, -1, -1, false, { parsed_msg["@message"] })
-		if parsed_msg["change"] then
-			vim.api.nvim_buf_set_lines(popup.bufnr, -1, -1, false, {
-				"Resource: "
-					.. parsed_msg["change"]["resource"]["resource_name"]
-					.. ", address: "
-					.. parsed_msg["change"]["resource"]["resource"]
-					.. ", action: "
-					.. parsed_msg["change"]["action"],
-			})
-			if parsed_msg["change"]["reason"] then
-				vim.api.nvim_buf_set_lines(popup.bufnr, -1, -1, false, { "Reason: " .. parsed_msg["change"]["reason"] })
-			end
-		end
-	end
+    for _, v in ipairs(results) do
+        local parsed_msg = vim.json.decode(v)
+        vim.api.nvim_buf_set_lines(popup.bufnr, -1, -1, false, { parsed_msg["@message"] })
+        if parsed_msg["change"] then
+            vim.api.nvim_buf_set_lines(popup.bufnr, -1, -1, false, {
+                "Resource: "
+                    .. parsed_msg["change"]["resource"]["resource_name"]
+                    .. ", address: "
+                    .. parsed_msg["change"]["resource"]["resource"]
+                    .. ", action: "
+                    .. parsed_msg["change"]["action"],
+            })
+            if parsed_msg["change"]["reason"] then
+                vim.api.nvim_buf_set_lines(popup.bufnr, -1, -1, false, { "Reason: " .. parsed_msg["change"]["reason"] })
+            end
+        end
+    end
+end
+
+local function terraform_validate()
+    local results = {}
+    Job:new({
+        command = "terraform",
+        args = { "validate", "-json" },
+        on_exit = function(j, _)
+            for _, line in ipairs(j:result()) do
+                table.insert(results, line)
+            end
+        end,
+    }):sync(99999) -- Needs a long time if the directory has tons of files
+
+    local fixed_table = table.concat(results, "\n")
+    local parsed_msg = vim.json.decode(fixed_table, {})
+    if parsed_msg["valid"] then
+        vim.notify("Terraform is valid")
+    else
+        vim.notify("There are " .. parsed_msg["error_count"] .. " error(s) in your file")
+    end
 end
 
 local M = {}
 
 M.plan = function()
-	if not file_extension() then
-		return
-	end
-	-- opens popup
-	popup:mount()
-	-- runs tf plan
-	terraform_plan()
-	-- unmount component when cursor leaves buffer
-	popup:on(event.BufLeave, function()
-		popup:unmount()
-	end)
+    if not file_extension() then
+        return
+    end
+    -- opens popup
+    popup:mount()
+    -- runs tf plan
+    terraform_plan()
+    -- unmount component when cursor leaves buffer
+    popup:on(event.BufLeave, function()
+        popup:unmount()
+    end)
+end
+
+M.validate = function()
+    if not file_extension() then
+        return
+    end
+    terraform_validate()
 end
 
 return M
--- lua/terraform/init.lua
+++ lua/terraform/init.lua
@@ -1,16 +1,18 @@
 -- Module definition
 local M = {}
 local telescope_state = require("terraform.telescope_picker")
-local terraform_plan = require("terraform.actions")
-
+local terraform_act = require("terraform.actions")
 
 M.plan = function()
-  terraform_plan.plan()
+    terraform_act.plan()
 end
 
-
 M.state = function()
-  telescope_state.run()
+    telescope_state.run()
+end
+
+M.validate = function()
+    terraform_act.validate()
 end
 
 return M
